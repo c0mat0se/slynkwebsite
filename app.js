a04f0d54b0b332c4bd556aca695b1d249c0ec7e2
tjyaz27 <tjyaz3@gmail.com>
2016-01-17 22:20:18 -0500

    Background Fix and Script Animation Changes
    Updated script so that animation only runs once then waits 10 seconds
    before going again. Now can focus on the actual frames.

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+var i = 0;
+
 function startAnimation() { 
     var frames = document.getElementById("startLogo").children;
     var frameCount = frames.length;
-    var i = 0;
-    setInterval(function () { 
+    var myTimer= setInterval(function() {
+        if(i==80) {
+            clearInterval(myTimer);
+            idleAnimation();
+        } else {  
         frames[i % frameCount].style.display = "none";
         frames[++i % frameCount].style.display = "block";
+        }
     }, 70);
 }
 
+function idleAnimation() {
+   var iTimer = setInterval(function() {
+        i = 0;
+        startAnimation();
+        clearInterval(iTimer);        
+    }, 10000);
+}
 $(document).ready(startAnimation());
 
